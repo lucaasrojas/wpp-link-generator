1994f6be9bcc04113ca9d076ac07df18e0b8a531
Lucas Rojas <lucas.rojas@live.com.ar>
2024-01-15 21:28:12 -0300

    Install MUI and improve layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,77 @@
 import { useState } from "react";
 import "./App.css";
 import QRCode from "react-qr-code";
-
+import { Grid, Button, TextField } from '@mui/material'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 function App() {
+
   const [number, setNumber] = useState<string>();
 
   return (
-    <div className="App">
-      <h2>Genera un link de contacto rapido para tu whatsapp</h2>
-      <label htmlFor="numberInput">Ingresa el numero</label>
-      <div>
-        <input
-          id="numberInput"
-          value={number}
-          onChange={(e) =>
-            setNumber(
-              e.target.value.replaceAll("-", "").replaceAll(" ", "").trim(),
-            )
-          }
-          // placeholder="Ingresa el numero con codigo de area"
-        />
+    <ThemeProvider theme={darkTheme}>
+      <div className="App">
+        <Grid container spacing={4}>
+          <Grid sm={8}>
+            <Grid xs={12} textAlign={"left"}>
+              <h1>WPP Me</h1>
+              <h3>Genera un link de contacto rapido para tu whatsapp</h3>
+            </Grid>
+            <Grid item xs={12} style={{ display: "flex", justifyContent: "center", gap: 20, alignItems: "center" }}>
+              <TextField id="numberInput" label="Número" variant="standard" value={number} style={{ color: "white", width: "100%" }}
+                onChange={(e) =>
+                  setNumber(
+                    e.target.value.replaceAll("-", "").replaceAll(" ", "").trim(),
+                  )
+                } />
+
+
+
+            </Grid>
+            <Grid item xs={12} textAlign={"left"}>
+              <p>
+                Los links aparecen automaticamente una vez ingreses al menos 6 digitos
+              </p>
+            </Grid>
+            {number && number.length > 6 && (
+              <Grid container xs={12} >
+                <Grid item xs={12} sm={6}>
+
+                  <Button variant="contained" onClick={() => window.open(`https://api.whatsapp.com/send/?phone=${number}&text&type=phone_number&app_absent=1`, "_blank")}>Chatear por la app</Button>
+                </Grid>
+
+                <Grid  item xs={12} sm={6}>
+
+                  <Button variant="contained" onClick={() => window.open(`https://web.whatsapp.com/send/?phone=${number}&text&type=phone_number&app_absent=1`, "_blank")}>Chatear por la web</Button>
+
+                </Grid>
+              </Grid>
+            )}
+          </Grid>
+
+          <Grid container sm={4} style={{display: "flex", placeItems: "center", placeContent:"center"}}>
+            {number && number.length > 6 && (
+
+
+              <Grid item xs={12} p={4}>
+                <QRCode
+                  size={256}
+                  style={{ margin: "auto" }}
+                  value={`https://api.whatsapp.com/send/?phone=${number}&text&type=phone_number&app_absent=1`}
+                />
+              </Grid>
+
+
+            )}
+          </Grid>
+
+        </Grid>
       </div>
-      <p>
-        Los links aparecen automaticamente una vez ingreses el numero de
-        contacto
-      </p>
-      {number && number.length > 6 && (
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            placeContent: "center",
-          }}
-        >
-          <a
-            href={`https://api.whatsapp.com/send/?phone=${number}&text&type=phone_number&app_absent=1`}
-            target="_blank"
-            rel="noreferrer"
-          >
-            Chatear por la app
-          </a>
-          <QRCode
-            size={256}
-            style={{ maxWidth: "200px", margin: "auto" }}
-            value={`https://api.whatsapp.com/send/?phone=${number}&text&type=phone_number&app_absent=1`}
-          />
-          <a
-            href={`https://web.whatsapp.com/send/?phone=${number}&text&type=phone_number&app_absent=1`}
-            target="_blank"
-            rel="noreferrer"
-          >
-            Chatear por web
-          </a>
-          
-        </div>
-      )}
-    </div>
+    </ThemeProvider>
   );
 }
 
