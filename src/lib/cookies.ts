43867310cf3727fc047f222ba19bd0b9e6577e37
Lucas Rojas <lucas.rojas@live.com.ar>
2025-07-29 00:03:47 -0300

    fix type issue

--- src/lib/cookies.ts
+++ src/lib/cookies.ts
@@ -8,5 +8,5 @@ export const setCookie = (name: string, value: string, days: number) => {
 export const getCookie = (name: string) => {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
+    if (parts.length === 2) return parts.pop()?.split(';').shift();
 };
